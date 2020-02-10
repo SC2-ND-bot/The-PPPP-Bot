d583902d200c51752a0ec856af2456ac76bc0492
Hsanch <hsanch97@gmail.com>
2020-02-09 20:48:24 -0500

    First Working Version

--- PPPP.py
+++ PPPP.py
@@ -10,32 +10,18 @@ from sc2.position import Point2, Point3
 class PPPP(sc2.BotAI):
     def __init__(self):
         self.path_coord_dict = {}
-
-    def go_to_work(self, worker, working_locations):
-        print(len(self.path_coord_dict))
-        
+        self.working_locations = {}
 
     async def on_step(self, iteration):
         if iteration == 0:
             await self.chat_send("(probe)(pylon)(cannon)(cannon)(gg)")
             await self.build_coord_dict()
-            self.go_to_work(None, None)
-            for resource in self.resources:
+            bases = self.townhalls.ready
+            gas_buildings = self.gas_buildings.ready
+            for resource in bases | gas_buildings:
                 x_coord = math.floor(resource.position.x)
                 y_coord = math.floor(resource.position.y)
-                working_locations = {}
-                working_locations[(x_coord, y_coord)] = resource
-
-
-                # Important Resource Properties
-                # is_mineral_field
-                # is_vespene_geyser
-                # vespene_contents
-                # mineral_contents
-                # ideal_harvesters
-                # assigned_harvesters
-                # surplus_harvesters
-                
+                self.working_locations[(x_coord, y_coord)] = resource
 
         if not self.townhalls:
             # Attack with all workers if we don't have any nexuses left, attack-move on enemy spawn (doesn't work on 4 player map) so that probes auto attack on the way
@@ -47,43 +33,23 @@ class PPPP(sc2.BotAI):
         
         ################# This is the logic relevant for milestone 1 #################
         
-        # for worker in self.workers:
-        #     if worker.is_idle:
-
-                # DEFINE: go_to_work(worker, working_locations):
-                # 
-                # Search through Adjencency List to find the first (x, y) that
-                # has an entry in the working_locations dictionary and also 
-                # fits the following criteria:
-                #   it does not have surplus workers
-                #   it hasn't been exhausted. 
-                # Once the nearest resource (that fits the criteria) is found, then
-                # utilize the resource object from the working_locations dictionary
-                # anbd call 'self.do(worker.gather(resource))'.
-
-                # go_to_work(worker, working_locations)
+        for worker in self.workers:
+            if worker.is_idle:
+                print("calling go to work")
+                self.go_to_work(worker)
 
         ######################################################################################################
 
         # If we have no pylon, build one near starting nexus
-        if self.supply_left < 4 and self.already_pending(PYLON) == 0:
+        if len(self.structures(PYLON)) < 1 and self.already_pending(PYLON) == 0:
             if self.can_afford(PYLON):
                 await self.build(PYLON, near=self.main_base_ramp.protoss_wall_pylon)
 
-
         # Make probes until we have 16 in each base
         for nexus in nexuses:
-            if self.supply_workers < 22 * len(nexuses) and nexus.is_idle:
+            if self.supply_workers < 22 and nexus.is_idle:
                 if self.can_afford(PROBE):
                     self.do(nexus.train(PROBE), subtract_cost=True, subtract_supply=True)
-        
-        abilities = await self.get_available_abilities(nexuses)
-        for loop_nexus, abilities_nexus in zip(nexuses, abilities):
-            if AbilityId.EFFECT_CHRONOBOOSTENERGYCOST in abilities_nexus:
-                for nexus in nexuses:
-                    if not nexus.is_idle and not nexus.has_buff(BuffId.CHRONOBOOSTENERGYCOST):
-                        self.do(loop_nexus(AbilityId.EFFECT_CHRONOBOOSTENERGYCOST, nexus))
-                        break
 
     async def build_coord_dict(self):
         path_matrix = self.game_info.pathing_grid.data_numpy
@@ -99,32 +65,55 @@ class PPPP(sc2.BotAI):
                             self.path_coord_dict[(i,j)].append(z)
         print("build_coord_dict() FINISHED!")
 
-	async def bfs(self, root):
-		visited = [root]
-		queue = [root]
-		
-		while queue:
-			node = queue.pop(0)
-			for neighbor in self.path_coord_dict[node]:
-				if neighbor not in visited:
-					if self.working_locations.get(neighbor) is not None:
-						resource = self.working_locations[neighbor]
-						is_not_full = (resource.ideal_harvesters - resource.assigned_harvesters) > 0
-						if resource.is_mineral_field:
-							is_not_empty = resource.mineral_contents > 0
-						elif resource.is_vespene_geyser:
-							is_not_empty = resource.vespene_contents > 0
-							
-						if is_not_full and is_not_empty:
-							return neighbor
-					visited.append(neighbor)
-					queue.append(neighbor)
+    def go_to_work(self, worker):
+        print("inside go to work")
+        x_coord = math.floor(worker.position.x)
+        y_coord = math.floor(worker.position.y)
+        worker_coords = (x_coord, y_coord)
+
+        working_location = self.bfs(worker_coords)
+        print("working location: ", working_location)
+        if (working_location is not None):
+            print("working_location in self.structures(NEXUS): ", self.working_locations[working_location] in self.structures(NEXUS))
+            if (self.working_locations[working_location] in self.structures(NEXUS)):
+                # print("self.mineral_field: ", self.mineral_field)
+                for mineral in self.mineral_field:
+                    if mineral.distance_to(working_location) <= 8:
+                        print("found minerals close to Nexus")
+                        self.do(worker.gather(mineral))
+                        break
+            else:
+                self.do(worker.gather(self.working_locations[working_location]))
+        else:
+            print("working location is None")
+            
+    def bfs(self, root):
+        visited = [root]
+        queue = [root]
+        while queue:
+            node = queue.pop(0)
+            for neighbor in self.path_coord_dict[node]:
+                if neighbor not in visited:
+                    if self.working_locations.get(neighbor) is not None:
+                        resource = self.working_locations[neighbor]
+                        is_not_full = (resource.ideal_harvesters - resource.assigned_harvesters) > 0
+                        is_not_empty = True
+
+                        if resource.is_mineral_field:
+                            is_not_empty = resource.mineral_contents > 0
+                        elif resource.is_vespene_geyser:
+                            is_not_empty = resource.vespene_contents > 0
+                        if True:
+                            return neighbor
+                        
+                    visited.append(neighbor)
+                    queue.append(neighbor)
 
 def main():
     sc2.run_game(
         sc2.maps.get("AcropolisLE"),
         [Bot(Race.Protoss, PPPP(), name="CheeseCannon"), Computer(Race.Protoss, Difficulty.VeryEasy)],
-        realtime=False,
+        realtime=True,
     )
 
 
