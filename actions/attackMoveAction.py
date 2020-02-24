8be5d3f8d674636285c135bc45ab7f87d0a7d375
jbackm2 <jbackm2@uic.edu>
2020-02-23 22:16:00 -0500

    Added functionality towards goal of GOAP implementation

---
+++ actions/attackMoveAction.py
@@ -0,0 +1,38 @@
+from actions.action import Action
+
+class AttackMoveAction(Action):
+	def __init__(self, agent, gameObject):
+		# add preconditions
+		# add effects
+
+		self.agent = agent
+		self.gameObject = gameObject
+		self.cost = calcCost()
+		self.attackLocation = None
+		self.moving = False
+		
+		addPrecondition("can_attack", True)
+		addPrecondition("is_active", False)
+
+	#def calcCost(self):
+		# determine the cost for this action
+		# will be used during planning
+
+	def reset(self):
+		self.attackLocation = None
+		self.moving = False
+
+	def canBeInterrupted(self):
+		return True
+
+	def checkProceduralPrecondition(self):
+		# This should generate a valid attackLocation
+		attackLocation = self.gameObject.enemy_start_locations[0]
+		self.attackLocation = attackLocation
+
+	def isDone(self):
+		return not self.moving
+
+	def perform(self):
+		self.gameObject.do(self.agent.attack(self.attackLocation))
+		#self.agent.is_moving
