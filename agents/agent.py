16e6e0647a36a8d9ba18de69d047acf267c7e3e7
Hsanch <hsanch97@gmail.com>
2020-03-19 20:08:31 -0400

    Fix goap and add first working action

--- agents/agent.py
+++ agents/agent.py
@@ -6,15 +6,17 @@ from FSM.states import AgentStates
 
 class Agent:
 
-	def __init__(self, unit, worldState):
-		self.unit = unit
-		self.worldState = worldState
+	def __init__(self):
+		self.unit = None
+		self.worldState = {}
 		self.stateMachine = StateMachine()
+		self.planner = None
 
 		self.availableActions = []
 		self.currentActions = []
+
 		self.stateMachine.add_state("IDLE_STATE", self.idleStateHandler)
-		self.stateMachine.add_state("PERFORM_ACTION_STATE", self.performActionStateHandler)
+		self.stateMachine.add_state("PERFORM_ACTIONS_STATE", self.performActionsStateHandler)
 		self.stateMachine.add_state("END_STATE", self.endStateHandler, True)
 
 		self.stateMachine.set_start("IDLE_STATE")
@@ -22,57 +24,49 @@ class Agent:
 		# TODO: Create DataProvider which feeds the agents goals and state information
 		# self.dataProvider = DataProvider()
 
-		# TODO: Define AgentPlanner class
-		# self.planner = AgentPlanner()
+	def hasValidPlan(self):
+		raise NotImplementedError
 
 	def idleStateHandler(self, gameObject):
-		# TODO: getGoal() should return a key:string -> value:boolean pair
 		print('in idle state')
 		# goal = self.dataProvider.getGoal()
-		goal = ('DISRUPT_ENEMY_ECONOMY', True)
+		goal = ('movingTowardsEnemy', True)
 
-		# TODO: Should return all relevant world information 
+		# TODO: Should return all relevant world information
 		# worldState = self.dataProvider.getWorldState()
 		worldState = None
 
 		# agent, actions, state, goal
-		plan = self.planner.plan(self, self.availableActions, worldState, goal)
+		plan = self.planner.plan(self, self.availableActions, self.worldState, goal)
 
 		if plan is not None:
 			self.currentActions = plan
-			return "PERFORM_ACTION_STATE"
+			return "PERFORM_ACTIONS_STATE"
 		else:
 			print('No valid plan found, agent will remain idle')
 			return "IDLE_STATE"
 
-	def performActionStateHandler(self, gameObject):
-		if not self.hasActivePlan():
+	def performActionsStateHandler(self, gameObject):
+		print('in perform actions state')
+		if not self.hasValidPlan():
 			return "IDLE_STATE"
 
-		action = self.currentActions[0]
-		if action.isFinished():
-			self.currentActions.pop(0)
-
-		if self.hasActivePlan():
-			newAction = self.currentActions[0]
-			success = newAction.perform()
+		success = False
+		if self.hasValidPlan():
+			for index, action in enumerate(self.currentActions):
+				firstAction = index == 0
+				action.perform(firstAction)
+			success = True
 
 			if not success:
-				print('Action failed, going back to idle state')
+				print('Queuing Actions failed, going back to idle state')
 				return "IDLE_STATE"
-				
-			return "PERFORM_ACTION_STATE"
 
-	def hasActivePlan(self, gameObject):
-		return len(self.currentActions) > 0
+			return "PERFORM_ACTIONS_STATE"
 
-	# def loadActions(self):
+	def loadActions(self):
+		raise NotImplementedError
 
 	# # Still unsure if we require this state and handler
 	def endStateHandler(self):
 		print('reached end state')
-
-	def updateWorldState(worldState):
-		self.worldState = worldState
-		# import copy
-		# self.worldState = copy.deepcopy(worldState)
